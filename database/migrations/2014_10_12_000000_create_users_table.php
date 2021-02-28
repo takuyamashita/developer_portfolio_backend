8dbf90cee1bc7450a08f591dc4964c705e2411ea
takuyamashita <taku.pro-myfam@ezweb.ne.jp>
2021-02-28 22:23:51 +0900

    backend

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
+            $table->string('name',100)->unique();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
